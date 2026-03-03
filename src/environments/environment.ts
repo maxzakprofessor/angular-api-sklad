600166765893abcf35f213b4da765d9448010837
Maxat Zakiryanov <m.zakiryanov70@gmail.com>
2026-03-03 11:13:12 +0500

    Koyeb server assigned and started

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (import.meta as any).env?.VITE_API_URL || 'https://drf-api-stock-ai.onrender.com/api',
+  apiUrl: (import.meta as any).env?.VITE_API_URL || 'https://thundering-chantal-zakiryanov-66760eab.koyeb.app/api/',
   authType: 'Bearer',
   tokenField: 'access'
 };
