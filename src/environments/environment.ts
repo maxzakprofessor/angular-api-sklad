1818007c16ea3a4a19b2b19ffc99ba7832dc3536
Maxat Zakiryanov <m.zakiryanov70@gmail.com>
2026-03-03 11:16:49 +0500

    again // removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: (import.meta as any).env?.VITE_API_URL || 'https://thundering-chantal-zakiryanov-66760eab.koyeb.app/api/',
+  apiUrl: (import.meta as any).env?.VITE_API_URL || 'https://thundering-chantal-zakiryanov-66760eab.koyeb.app/api',
   authType: 'Bearer',
   tokenField: 'access'
 };
